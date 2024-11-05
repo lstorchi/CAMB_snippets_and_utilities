137dc153bf0d685d86edcfd82746f37ec0a56ffb
Loriano Storchi <loriano@storchi.org>
2024-11-05 10:21:08 +0100

    test script

---
+++ CAMB_python.py
@@ -0,0 +1,34 @@
+import sys, platform, os
+import matplotlib
+from matplotlib import pyplot as plt
+import numpy as np
+import camb
+from camb import model, initialpower
+
+#Set up a new set of parameters for CAMB
+#The defaults give one massive neutrino and helium set using BBN consistency
+pars = camb.set_params(H0=67.5, ombh2=0.022, omch2=0.122, mnu=0.06, omk=0, tau=0.06,  
+                       As=2e-9, ns=0.965, halofit_version='mead2020', lmax=11000)
+
+pars.set_accuracy(AccuracyBoost=2, lSampleBoost=2.0, lAccuracyBoost=2.0, DoLateRadTruncation=False) 
+
+pars.set_matter_power(kmax=10.0, k_per_logint=130, nonlinear=True)
+
+pars.set_for_lmax(11000, lens_potential_accuracy=8, lens_margin=2050)
+
+#Non-Linear spectra (Halofit)
+pars.NonLinear = model.NonLinear_both
+
+# get results and compute 
+results = camb.get_results(pars)
+results.calc_power_spectra(pars)
+
+
+
+# parameters can also be read from text .ini files, for example this sets up a best-fit 
+# Planck 2018 LCDM cosmology (base_plikHM_TTTEEE_lowl_lowE_lensing). 
+# [Use planck_2018_acc.ini if you need high-ell and/or accurate BB and CMB lensng spectra at beyond-Planck accuracy]
+pars = camb.read_ini('high_acc.ini')
+
+# view parameter objects using print(), or use pickle or repr to save and restore
+print(pars)
